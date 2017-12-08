6a68e8f7725005d1fb1f615a70f7485282e2bf5a
MohamedHamdy231995 <mohamedhamdy231995@gmail.com>
2017-12-08 16:09:23 +0200

    all Done

---
+++ Vec2.js
@@ -0,0 +1,63 @@
+﻿var Vec2 = function (x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.hamada = [];
+    var that = this;
+    
+    this.add = function (a, b) {
+        ///<param name='a' type="Vec2">first vector </param>
+        ///<param name='b' type="Vec2">second vector </param>
+        b = b || that;
+        this.x = a.x + b.x;
+        this.y = a.y + b.y;
+        var v = new Vec2(a.x+b.x, a.y+b.y);
+        return v;
+
+    };
+    
+    this.sub = function (a, b) {
+        ///<param name='a' type="Vec2">first vector </param>
+        ///<param name='b' type="Vec2">second vector </param>
+        b = b || that;
+
+        this.x = a.x - b.x;
+        this.y = a.y - b.y;
+        var v = new Vec2(a.x - b.x, a.y - b.y);
+        return v;
+
+    };
+
+    this.dot = function (a, b) {
+        ///<param name='a' type="Vec2">first vector </param>
+        ///<param name='b' type="Vec2">second vector </param>
+        b = b || this;
+        return a.x * b.x + a.y * b.y;
+    };
+    this.scale = function (s) {
+        return new Vec2(this.x*s,this.y*s);
+    }
+    this.magnitude = function () {
+        return Math.sqrt(that.x * that.x + that.y * that.y);
+    };
+    this.magnitude2 = function () {
+        return (that.x * that.x + that.y * that.y);
+    };
+
+
+    this.normalize = function () {
+        var v = this.normalized();
+        this.x = v.x;
+        this.y = v.y;
+        return this;
+    };
+
+    this.normalized = function () {
+        var m = that.magnitude();
+        var v = new Vec2(this.x/m, this.y/m);
+        return v;
+    };
+    this.normal = function () {
+        return (new Vec2(this.y, -this.x)).normalized();
+    }
+}
+var vec2 = Vec2;
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 ﻿/// <reference path="p2.js" />
+/// <reference path="Vec2.js" />
 
 
 
@@ -16,20 +17,22 @@ var world = new p2.World({
 
 // creat planets
 {
-    let body = new p2.Body({ position: [500, 150], gravity: [0, 9.8] });
+    let body = new p2.Body({mass:0, position: [500, 150], gravity: [0,0]});
     let shape = new p2.Circle({ radius: 60 });
     body.addShape(shape);
     world.addBody(body);
     planets.push(body);
+    //let constrain = new p2.DistanceConstraint(body, body);
+    //world.addConstraint(constrain);
 
-    body = new p2.Body({ position: [800, 400], gravity: [0, 9.8] });
-    shape = new p2.Circle({ radius: 80 });
+    body = new p2.Body({ position: [800, 400], gravity: [0, 0] });
+    shape = new p2.Circle({ radius: 60 });
     body.addShape(shape);
     world.addBody(body);
     planets.push(body);
 
-    body = new p2.Body({ position: [200, 400], gravity: [0, 9.8] });
-    shape = new p2.Circle({ radius: 80 });
+    body = new p2.Body({ position: [200, 400], gravity: [0, 0] });
+    shape = new p2.Circle({ radius: 60 });
     body.addShape(shape);
     world.addBody(body);
     planets.push(body);
@@ -55,7 +58,7 @@ function render() {
     
     canvas.width = canvas.width;
     for (var i = 0; i < planets.length; i++) {
-        console.log("i=" + i);
+        //console.log("i=" + i);
         ctx.beginPath();
         ctx.arc(planets[i].position[0], planets[i].position[1], 60, 0, 2 * 3.1415926);
         ctx.closePath();
@@ -63,8 +66,9 @@ function render() {
     }
 
     for (var i = 0; i < bodies.length; i++) {
-        console.log("i=" + i);
+        //console.log("i=" + i);
         ctx.beginPath();
+        CalculateAllForce(bodies[i]);
         ctx.arc(bodies[i].position[0],bodies[i].position[1], 5, 0, 2 * 3.1415926);
         ctx.closePath();
         ctx.fill();
@@ -73,15 +77,42 @@ function render() {
 }
 
 document.onmousedown = function (e) {
-    console.log("down");
+    //console.log("down");
     mouseBody.position[0] = e.offsetX;
     mouseBody.position[1] = e.offsetY;
-    let body = new p2.Body({ position: [e.offsetX, e.offsetY] });
+    let body = new p2.Body({mass: 300, position: [e.offsetX, e.offsetY] });
     let shape = new p2.Circle({ radius: 5 });
     body.addShape(shape);
     world.addBody(body);
     bodies.push(body);
+    
+    CalculateAllForce(body);
+
+
 };
+
+function CalculateAllForce(obj) {
+    for (var i = 0; i < planets.length; i++) {
+        Calculate(planets[i], obj);
+    }
+}
+function Calculate(planet, obj) {
+
+
+    var direction = new Vec2().sub(new Vec2(planet.position[0], planet.position[1]), new Vec2(obj.position[0], obj.position[1])).normalize();
+    direction = direction.scale(10000);
+    var dist2 =Math.abs(
+                 
+                       Math.pow((planet.position[0]-obj.position[0]),2)+
+                       Math.pow((planet.position[1]-obj.position[1]),2)
+    );
+
+    
+    var force =500 * obj.mass / dist2;
+    obj.applyForce([force * direction.x, force * direction.y]);
+   // obj.applyForce([force * direction.x, force * direction.y], [planet.position[0], planet.position[1]]);
+    
+}
 
 
 requestAnimationFrame(Update);
